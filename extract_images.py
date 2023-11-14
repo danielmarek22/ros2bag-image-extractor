0d05edcef7a54ea3747b281192dbad65bf56d660
BSH <bsh@pop-os.localdomain>
2023-11-14 14:50:20 +0100

    add a case for depth files

--- extract_images.py
+++ extract_images.py
@@ -9,7 +9,7 @@ from sensor_msgs.msg import Image
 import argparse
 
 
-def convert_db3_to_images(db_file, output_dir):
+def convert_db3_to_images(db_file, output_dir, image_type):
     # Create output directory if it doesn't exist
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -32,29 +32,47 @@ def convert_db3_to_images(db_file, output_dir):
         topic, msg, t = reader.read_next()
 
         # Check if the message is an image
-        if topic.endswith('/raw'):
-            try:
-                # Deserialize the message
-                deserialized_msg = deserialize_message(msg, Image)
-                deserialized_img = np.asarray(deserialized_msg.data)
+        if image_type == 'raw':
+            if topic.endswith('/raw'):
+                try:
+                    # Deserialize the message
+                    deserialized_msg = deserialize_message(msg, Image)
+                    deserialized_img = np.asarray(deserialized_msg.data)
 
-                # Get back the original dimensions of image
-                img_shape = (deserialized_msg.height, deserialized_msg.width, 3)
-                deserialized_img = np.reshape(deserialized_img, img_shape)
- 
-                # Extract the timestamp
-                timestamp = deserialized_msg.header.stamp
+                    # Extract the timestamp for filename creation
+                    timestamp = deserialized_msg.header.stamp
+                    image_filename = os.path.join(output_dir, 'image_{}_{}.jpg'.format(timestamp.sec, timestamp.nanosec))
 
-                # Save the image to a file
-                image_filename = os.path.join(output_dir, 'image_{}_{}.jpg'.format(timestamp.sec, timestamp.nanosec))
-                cv2.imwrite(image_filename, deserialized_img)
-                cv2.imshow("deserialized img",deserialized_img)
-                # cv2.waitKey()
+                    # Get back the original dimensions of image
+                    img_shape = (deserialized_msg.height, deserialized_msg.width, 3)
+                    deserialized_img = np.reshape(deserialized_img, img_shape)
 
-                print('Image saved to {}'.format(image_filename))
+                    # Save the image to a file
+                    cv2.imwrite(image_filename, deserialized_img)
 
-            except Exception as e:
-                print('Error processing image: {}'.format(e))
+
+                except Exception as e:
+                    print('Error processing image: {}'.format(e))
+
+        if image_type == 'depth':
+            if topic.endswith('/depth'):
+                try:
+                    # Deserialize the message
+                    deserialized_msg = deserialize_message(msg, Image)
+                    deserialized_img = np.asarray(deserialized_msg.data)
+
+                    # Extract the timestamp for filename creation
+                    timestamp = deserialized_msg.header.stamp
+                    image_filename = os.path.join(output_dir, 'image_{}_{}.jpg'.format(timestamp.sec, timestamp.nanosec))
+
+                    img_shape = (deserialized_msg.height, deserialized_msg.width, 2)
+                    deserialized_img = np.reshape(deserialized_img, img_shape)
+                    cv2.imwrite(image_filename, deserialized_img[:,:, 1])
+                    print('Image saved to {}'.format(image_filename))
+
+                except Exception as e:
+                    print('Error processing image: {}'.format(e))
+
 
 if __name__ == '__main__':
     # Specify argument parser for getting paths and types of stored data
@@ -63,6 +81,8 @@ if __name__ == '__main__':
                     help='path to the rosbag db3 file')
     parser.add_argument('output_folder', type=str,
             help='path to the rosbag db3 file')
+    parser.add_argument('image_type', choices=['raw', 'depth'], type=str,
+                        help='choose what type of image is extracted')
 
     args = parser.parse_args()        
 
@@ -72,5 +92,8 @@ if __name__ == '__main__':
     # Specify the output directory for images
     output_directory = args.output_folder
 
+    # Specify the image type
+    img_type = args.image_type
+
     # Convert the .db3 file to images
-    convert_db3_to_images(db_file_path, output_directory)
+    convert_db3_to_images(db_file_path, output_directory, img_type)
